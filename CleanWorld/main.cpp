3a383c90f9f25d799ef6b73ebfe72457cf773f49
DimoDM <dimo_marino@abv.bg>
2020-10-02 22:36:58 +0300

    create window

--- CleanWorld/main.cpp
+++ CleanWorld/main.cpp
@@ -1,8 +1,29 @@
-﻿#include <iostream>
+﻿#include"Game.h"
 
-
-int main()
+Game* game = nullptr;
+int main(int argc, char** argv)
 {
-	std::cout << "asd";
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+	Uint32 frameStart;
+	int frameTime;
+
+	game = new Game();
+	game->init("Display", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+	
+	while (game->running()) {
+		frameStart = SDL_GetTicks();
+		//std::cout << frameStart << std::endl;
+
+		game->handleEvents();
+		game->update();
+		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+		if (frameDelay > frameTime) {
+			SDL_Delay(frameDelay - frameTime);
+		}
+	}
+	system("pause");
 	return 0;
 }
